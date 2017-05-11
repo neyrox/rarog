fdebf07285b22ce87b96571583ec9a318679a90e
neyrox <y.stanislav.v@gmail.com>
2017-05-11 22:53:16 +0300

    Update operation implemented

---
+++ Engine/UpdateNode.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Engine
+{
+    public class UpdateNode: Node
+    {
+        public string TableName;
+        public List<string> ColumnNames;
+        public List<string> Values;
+        public List<ConditionNode> Conditions;
+
+        public UpdateNode(string tableName, List<string> columnNames, List<string> values, List<ConditionNode> conditions)
+        {
+            TableName = tableName;
+            ColumnNames = columnNames;
+            Values = values;
+            Conditions = conditions;
+        }
+    }
+}
