621a2001327c604884a95818ae570a9d6400ea09
Stanislav Yablonskiy <s.yablonskiy@pixonic.com>
2020-09-12 20:58:37 +0300

    Fix Integer >= Condition

---
+++ UnitTests/IntConditionTest.cs
@@ -0,0 +1,67 @@
+﻿using Engine;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class IntConditionTest
+    {
+        [TestMethod]
+        public void IntEqualToInt()
+        {
+            var condition = new ConditionIntegerEqual("5");
+            Assert.IsFalse(condition.Satisfies(4));
+            Assert.IsFalse(condition.Satisfies(-3));
+            Assert.IsTrue(condition.Satisfies(5));
+            Assert.IsFalse(condition.Satisfies(6));
+            Assert.IsFalse(condition.Satisfies(-7));
+        }
+
+        [TestMethod]
+        public void IntNotEqualToInt()
+        {
+            var condition = new ConditionIntegerNotEqual("5");
+            Assert.IsTrue(condition.Satisfies(-4));
+            Assert.IsTrue(condition.Satisfies(3));
+            Assert.IsFalse(condition.Satisfies(5));
+            Assert.IsTrue(condition.Satisfies(-6));
+            Assert.IsTrue(condition.Satisfies(7));
+        }
+
+        [TestMethod]
+        public void IntLessThenInt()
+        {
+            var condition = new ConditionIntegerLess("5");
+            Assert.IsTrue(condition.Satisfies(4));
+            Assert.IsFalse(condition.Satisfies(5));
+            Assert.IsFalse(condition.Satisfies(6));
+        }
+
+        [TestMethod]
+        public void IntLessOrEqualToInt()
+        {
+            var condition = new ConditionIntegerLessOrEqual("5");
+            Assert.IsTrue(condition.Satisfies(4));
+            Assert.IsTrue(condition.Satisfies(5));
+            Assert.IsFalse(condition.Satisfies(6));
+        }
+
+        [TestMethod]
+        public void IntGreaterThenInt()
+        {
+            var condition = new ConditionIntegerGreater("5");
+            Assert.IsTrue(condition.Satisfies(6));
+            Assert.IsFalse(condition.Satisfies(5));
+            Assert.IsFalse(condition.Satisfies(4));
+        }
+
+        [TestMethod]
+        public void IntGreaterOrEqualToInt()
+        {
+            var condition = new ConditionIntegerGreaterOrEqual("5");
+            Assert.IsTrue(condition.Satisfies(6));
+            Assert.IsTrue(condition.Satisfies(5));
+            Assert.IsFalse(condition.Satisfies(4));
+        }
+    }
+}
