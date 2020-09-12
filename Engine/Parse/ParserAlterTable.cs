f071b2fb408f8387881f0ec39918557f692e53fb
Stanislav Yablonskiy <s.yablonskiy@pixonic.com>
2020-09-12 22:42:41 +0300

    Code cleanup

--- Engine/Parse/ParserAlterTable.cs
+++ Engine/Parse/ParserAlterTable.cs
@@ -61,45 +61,5 @@ namespace Engine
 
             return new AlterTableAddColumnNode(tableName, columnName, dataType, length);
         }
-
-        private static void ConvertColumns(string[] tokens, List<string> columnNames, List<string> dataTypes, List<int> lengths, ref int pos)
-        {
-            while (pos < tokens.Length)
-            {
-                if (ParserCommon.AssertToken(")", tokens, pos))
-                {
-                    ++pos;
-                    break;
-                }
-
-                columnNames.Add(tokens[pos]);  // TODO: check column names
-                ++pos;
-                if (pos >= tokens.Length)
-                    break;
-
-                dataTypes.Add(tokens[pos]);  // TODO: check data types
-                ++pos;
-                if (pos >= tokens.Length)
-                    break;
-
-                if (ParserCommon.AssertToken("(", tokens, pos))
-                {
-                    ++pos;
-                    if (pos >= tokens.Length)
-                        break;
-                    lengths.Add(int.Parse(tokens[pos]));
-                    ++pos;
-                    if (ParserCommon.AssertToken(")", tokens, pos))
-                        ++pos;
-                    else
-                        break;
-                }
-                else
-                    lengths.Add(0);
-
-                if (ParserCommon.AssertToken(",", tokens, pos))
-                    ++pos;
-            }
-        }
     }
 }
